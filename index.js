610ab1af0e4262d5f5d2e7d96ce3d3848efcb44d
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-28 20:13:27 +0300

    assignment completed

--- index.js
+++ index.js
@@ -10,9 +10,56 @@ function createEmployeeRecord(array) {
 	};
 	return records;
 }
-function createEmployeeRecords() {
-	let employeeRecords = array.map((itemInArray) =>
-		createEmployeeRecord(itemInArray)
+function createEmployeeRecords(array) {
+	const records = array.map((data) => createEmployeeRecord(data));
+	return records;
+}
+function createTimeInEvent(employeeRecord, dateStamp) {
+	const [date, hour] = dateStamp.split(" ");
+	employeeRecord.timeInEvents.push({
+		type: "TimeIn",
+		hour: parseInt(hour, 10),
+		date,
+	});
+	return employeeRecord;
+}
+function createTimeOutEvent(employeeRecord, dateStamp) {
+	const [date, hour] = dateStamp.split(" ");
+	employeeRecord.timeOutEvents.push({
+		type: "TimeOut",
+		hour: parseInt(hour, 10),
+		date,
+	});
+	return employeeRecord;
+}
+function hoursWorkedOnDate(employeeRecord, specificDate) {
+	let timeInEvent = employeeRecord.timeInEvents.find(
+		(event) => event.date === specificDate
 	);
-	return employeeRecords;
+	let timeOutEvent = employeeRecord.timeOutEvents.find(
+		(event) => event.date === specificDate
+	);
+	let totalTimeWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+	// console.log(timeInEvent);
+	// console.log(timeOutEvent)
+	return totalTimeWorked;
+}
+function wagesEarnedOnDate(employeeRecord, specificDate) {
+	return parseFloat(
+		hoursWorkedOnDate(employeeRecord, specificDate) *
+			employeeRecord.payPerHour.toString()
+	);
+}
+function allWagesFor(employeeRecord) {
+	let payDates = employeeRecord.timeInEvents.map((e) => e.date);
+	let payOwedAllDates = payDates.reduce((allInfo, dates) => {
+		return allInfo + wagesEarnedOnDate(employeeRecord, dates);
+	}, 0);
+	return payOwedAllDates;
+}
+function calculatePayroll(employeeRecord) {
+	let empRecords = employeeRecord.reduce((allInfo, datesRecords) => {
+		return allInfo + allWagesFor(datesRecords);
+	}, 0);
+	return empRecords;
 }
