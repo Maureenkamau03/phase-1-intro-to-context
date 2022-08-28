e26e1166cda1e7a5b464d7c093440ae629aaf45b
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-28 13:30:44 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord(array) {
+	let records = {
+		firstName: array[0],
+		familyName: array[1],
+		title: array[2],
+		payPerHour: array[3],
+		timeInEvents: [],
+		timeOutEvents: [],
+	};
+	return records;
+}
+function createEmployeeRecords() {
+	let employeeRecords = array.map((itemInArray) =>
+		createEmployeeRecord(itemInArray)
+	);
+	return employeeRecords;
+}
